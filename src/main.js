be8cf6b15b2aa678630cb6ba8df61c2ee84c0063
Cruyun <1045671796@qq.com>
2018-04-18 16:27:39 +0800

    feat: add images & init scences

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import 'phaser';
 import BootScene from './BootScene';
 import MarioBrosScene from './MarioBrosScene';
 import TitleScene from './TitleScene';
-
+import LetterScence from './LetterScence';
+import GuideScence from './GuideScence';
+import IntroScence1 from './IntroScence1';
+import IntroScence2 from './IntroScence2';
+import IntroScence3 from './IntroScence3';
 
 let config = {
     type: Phaser.WEBGL,
@@ -21,10 +25,12 @@ let config = {
         // BootScene,
         // TitleScene,
         // MarioBrosScene,
+        LetterScence,
+        GuideScence,
+        IntroScence1,
+        IntroScence2,
+        IntroScence3
     ]
 };
 
 let game = new Phaser.Game(config);
-
-/*
-https://codepen.io/samme/pen/JMVBeV*/
